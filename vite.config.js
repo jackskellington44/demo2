b83a28295b3f7d3744ace861a3ef97b8e4e7f9e4
jackskellington44 <jackskellington4@duck.com>
2026-04-21 13:55:04 +0200

    Vite Config fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/demo1/",
+  base: "/demo2/",
   build: {
     rollupOptions: {
       input: {
